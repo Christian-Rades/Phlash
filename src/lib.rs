d8fa0e2f5ea3414ac211954a0cf2102bf7654a08
Christian Rades <c.rades@shopware.com>
2023-02-09 14:00:33 +0100

    feat: working minification

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
+use anyhow::{anyhow, Result};
 use ext_php_rs::prelude::*;
+
+use lightningcss::stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet};
 
 #[php_function]
 pub fn hello_world(name: &str) -> String {
-    format!("Hello, {}!", name);
+    format!("Hello, {}!", name)
+}
+
+#[php_function]
+pub fn transform(styles: &str) -> Result<String> {
+    let mut css =
+        StyleSheet::parse(styles, ParserOptions::default()).map_err(|err| anyhow!("{}", err))?;
+
+    css.minify(MinifyOptions::default())?;
+
+    let mut config = PrinterOptions::default();
+    config.minify = true;
+
+    Ok(css.to_css(config)?.code)
 }
 
 #[php_module]
